ef803694b2a1167728d611d7b7661d095bbf266e
Samuel Hammer <ham14447@spengergasse.at>
2014-11-03 20:41:01 +0100

    Implemented user settings and user display

--- src/at/hammer/adaptiveui/MainActivity.java
+++ src/at/hammer/adaptiveui/MainActivity.java
@@ -1,16 +1,46 @@
 package at.hammer.adaptiveui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import at.hammer.adaptiveui.R;
+import at.hammer.adaptiveui.SettingsActivity;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements OnSharedPreferenceChangeListener {
+	SharedPreferences sharedPref;
+	private String user;
+	
+	//define xml variables
+	TextView userTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPref.registerOnSharedPreferenceChangeListener(this);
+        
+        user = sharedPref.getString("users","red");
+        
+        init();
+        
+        userTextView.setText("Logged in as: " + user);
+	}
+	
+	/**
+	 * Initializes defined (xml) variables for use in code
+	 */
+	public void init() {
+		userTextView = (TextView) findViewById(R.id.userTextView);
+		
 	}
 
 	@Override
@@ -25,10 +55,16 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+		if(item.getItemId() == R.id.action_settings){
+            Intent settings = new Intent(this, SettingsActivity.class);
+            startActivity(settings);
+        }
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		user = sharedPref.getString("users", getResources().getString(R.string.red));
 	}
 }
---
+++ src/at/hammer/adaptiveui/SettingsActivity.java
@@ -0,0 +1,45 @@
+package at.hammer.adaptiveui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import at.hammer.adaptiveui.MainActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+
+	@Override
+    public void onBackPressed() {
+        Intent main = new Intent(this,MainActivity.class);
+        startActivity(main);
+    }
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent main = new Intent(this, MainActivity.class);
+			startActivity(main);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}

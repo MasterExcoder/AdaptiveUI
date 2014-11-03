0f4bb7790a995a30bf04d0b250849f7bd0689398
Samuel Hammer <ham14447@spengergasse.at>
2014-11-03 21:53:31 +0100

    WiFi state is now displayed and the download button changes its visibility automatically

--- src/at/hammer/adaptiveui/MainActivity.java
+++ src/at/hammer/adaptiveui/MainActivity.java
@@ -1,14 +1,19 @@
 package at.hammer.adaptiveui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import at.hammer.adaptiveui.R;
@@ -16,12 +21,16 @@ import at.hammer.adaptiveui.SettingsActivity;
 
 
 public class MainActivity extends Activity implements OnSharedPreferenceChangeListener {
+	//define variables
 	SharedPreferences sharedPref;
 	private String user;
+	boolean isWifiConn;
 	
 	//define xml variables
 	TextView userTextView;
 	LinearLayout layout;
+	TextView wifiTextView;
+	Button downloadButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +45,21 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
         init();
         setBackground();
         
-        userTextView.setText("Logged in as: " + user);
+        //display the user who is currently logged in
+        userTextView.setText(userTextView.getText() + user);
+        
+        //Check if device is connected to a WiFi network
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI); 
+        isWifiConn = networkInfo.isConnected();
+        if(isWifiConn) {
+        	wifiTextView.setText(wifiTextView.getText() + getString(R.string.wifiEnabledString));
+        	downloadButton.setVisibility(View.VISIBLE);
+        } 
+        else if (!isWifiConn) {
+        	wifiTextView.setText(wifiTextView.getText() + getString(R.string.wifiDisabledString));
+        	downloadButton.setVisibility(View.INVISIBLE);
+        }
 	}
 	
 	/**
@@ -45,6 +68,8 @@ public class MainActivity extends Activity implements OnSharedPreferenceChangeLi
 	public void init() {
 		userTextView = (TextView) findViewById(R.id.userTextView);
 		layout = (LinearLayout) findViewById(R.id.linearLayout1);
+		wifiTextView = (TextView) findViewById(R.id.wifiTextView);
+		downloadButton = (Button) findViewById(R.id.downloadButton);
 	}
 	
 	/**
